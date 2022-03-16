f31e47ce30f07e5987f0906ce4c21a02f5fd258f
Administrator <admin@example.com>
2022-03-15 22:52:08 -0600

    Formatting for selenium/hub docker

--- test.py
+++ test.py
@@ -10,9 +10,20 @@ import os
 class html_unittest(unittest.TestCase):
     def setUp(self):
         #host = os.environ['SELENIUM_REMOTE_HOST']
-        options = webdriver.ChromeOptions()
-        self.test_selenium_server_available()
-        self.driver = webdriver.Remote(command_executor='http://127.0.0.1:4444/wd/hub', desired_capabilities=options.to_capabilities())
+        username = os.getenv("BROWSERSTACK_USERNAME")
+        access_key = os.getenv("BROWSERSTACK_ACCESS_KEY")
+        browserstack_local = os.getenv("BROWSERSTACK_LOCAL")
+        browserstack_local_identifier = os.getenv("BROWSERSTACK_LOCAL_IDENTIFIER")
+        caps = {
+            'os': 'Windows',
+            'browser': 'chrome',
+            'browserstack.local': browserstack_local,
+            'browserstack.localIdentifier': browserstack_local_identifier,
+            'browserstack.user': username,
+            'browserstack.key': access_key
+        }
+
+        driver = webdriver.Remote(command_executor='https://hub-cloud.browserstack.com/wd/hub', desired_capabilities=caps)
     
     def test_parrafos(self):
         #webApp = os.environ['WEBAPP_HOST']
@@ -25,19 +36,6 @@ class html_unittest(unittest.TestCase):
     
     def tearDown(self):
         self.driver.close()
-
-    def test_selenium_server_available(self):
-        import requests
-        from requests.adapters import HTTPAdapter
-        from requests.packages.urllib3.util.retry import Retry
-
-        session = requests.Session()
-        retry = Retry(connect=55, backoff_factor=0.5)
-        adapter = HTTPAdapter(max_retries=retry)
-        session.mount('http://', adapter)
-        session.mount('https://', adapter)
-
-        session.get("http://localhost:4444/wd/hub")
 
 if __name__ == '__main__':
     unittest.main()
